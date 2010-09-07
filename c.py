5b22b90c52fb9339c7ac0f9a991c683d3cce7963
Ivan Tikhonov <kefeer@brokestream.com>
2010-09-08 02:52:45 +0600

    fixed numbers

--- c.py
+++ c.py
@@ -1,10 +1,10 @@
-bltin='; . drop ? n istack'.split()
+bltin='; . drop ? n istack @ ! + -'.split()
 
 from sys import stdin
 
 prog={}
 wid={'main':0}
-nums={0:0}
+nums=[]
 
 def makewid(w):
 	if w not in wid:
@@ -14,9 +14,8 @@ def makewid(w):
 def makenum(w):
 	if(w[0]=='_'): w=-int(w[1:])
 	else: w=int(w)
-	if w not in nums:
-		nums[w]=max(nums.values())+1
-	return nums[w]
+	nums.append(w)
+	return len(nums)-1
 
 for x in stdin:
 	w=x.split()
@@ -26,13 +25,15 @@ for x in stdin:
 		if '=' in x:
 			flags.append('JZ')
 			x=x.replace('=','')
-		if '+' in x:
+		if len(x)>1 and '+' in x:
 			flags.append('JP')
 			x=x.replace('+','')
-		if '-' in x:
+		if len(x)>1 and '-' in x:
 			flags.append('JN')
 			x=x.replace('-','')
-
+		if "'" in x:
+			flags.append('ZR')
+			x=x[1:]
 		if flags:
 			f='|'.join(flags)
 		else:
@@ -40,10 +41,11 @@ for x in stdin:
 
 		if x in bltin:
 			df.append("%s|CALL|BLTIN|%u"%(f,bltin.index(x)))
-		elif x.isdigit() or (x.startswith('_') and x[1:].isdigit()):
+		elif x.isdigit() or (x.startswith('-') and x[1:].isdigit()):
 			df.append("%s|CALL|NUM|%u"%(f,makenum(x)))
 		else:
 			df.append("%s|CALL|%u"%(f,makewid(x)))
+
 	prog[w[0]]=df
 	makewid(w[0])
 
@@ -53,5 +55,7 @@ for x,y in prog.items():
 	print 'static uint16_t prog_%u[]={%s|JMP, JZ|JN|JP|BLTIN|0|JMP};'%(wid[x],', '.join(y))
 
 print 'uint16_t *prog[]={%s};'%(','.join(['prog_%s'%x for x in range(0,len(wid))]))
-print 'uint32_t prog_numbers[]={%s};'%(','.join([str(x) for x in nums]))
+print 'uint64_t prog_numbers[]={%s};'%(','.join([str(x) for x in nums]))
+
+print 'char *prog_names[]={%s};'%(','.join(['"%s/%u"'%x for x in wid.items()]))
 
